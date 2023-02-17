48936e864c52bfc62f061193acb88b26e2f0ebb0
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2023-02-18 00:39:51 +0530

    cart & product

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {Navbar} from "/components/navbar";
+import {Navbar} from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Cart } from "./pages/cart/cart";
+
 
 function App() {
   return (
@@ -9,8 +12,8 @@ function App() {
      <Router>
       <Navbar/>
        <Routes>
-         <Route path='/' />
-         <Route path='/cart' />
+         <Route path='/' element= {<Shop/>} />
+         <Route path='/cart' element={<Cart/>}/>
        </Routes>
      </Router>
     </div>
