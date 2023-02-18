de2f66e667c1e1680c95343a039c862da4be8892
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2023-02-18 09:38:13 +0530

    context

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,16 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
 import "./navbar.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import {SiApplepodcasts } from "react-icons";
+
 
 export const Navbar = () => {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0;
   return (
     <div className="navbar">
+      <h3> Apple Store  </h3>
       <div className="links">
         <Link to="/"> Shop </Link>
+
         <Link to="/contact"> Contact </Link>
+
         <Link to="/cart">
           <ShoppingCart size={32} />
         </Link>
+        {isAuthenticated ? (
+          <li>
+            <button onClick={() => loginWithRedirect()}>Log In</button>
+          </li>
+        ) : (
+          <li>
+            <button
+              onClick={() =>
+                logout({ logoutParams: { returnTo: window.location.origin } })
+              }
+            >
+              Log Out
+            </button>
+          </li>
+        )}
       </div>
     </div>
   );
